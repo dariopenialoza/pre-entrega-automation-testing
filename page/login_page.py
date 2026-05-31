f8e83ca5966e42b54a69aee92edfcdbf8a3af8ea
dariopenialoza <dariopenialoza@gmail.com>
2026-05-31 19:17:06 -0300

    Se agrego inventory_page y test_inventory

--- page/login_page.py
+++ page/login_page.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
-      
+
+        # Selectores
         self.username_input = (By.ID, "user-name")
         self.password_input = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -2,22 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
 
+from page.inventory_page import InventoryPage
+from page.login_page import LoginPage
+
 @pytest.fixture
-def driver_logged(login_in_driver):
-    driver = login_in_driver
-    return driver
+def driver_logged(driver):
+    login_page = LoginPage(driver)
+    login_page.login("standard_user","secret_sauce")
+
+    return InventoryPage(driver)
 
 def test_inventory_title(driver_logged):
-    titulo = driver_logged.title
-    assert titulo == "Swag Labs", "El titulo de la pagina a la que se accede no es correcto"
+    titulo = driver_logged.obtener_titulo()
+    assert titulo == "Swag Labs", "El título de la página no es correcto."
 
 def test_productos_visibles(driver_logged):
-    productos = driver_logged.find_elements(By.CLASS_NAME,"inventory_item")
-    assert len(productos) > 0
+    productos = driver_logged.obtener_productos()
+    assert len(productos) > 0, "No se encontraron productos en el inventario."
 
 def test_ui_elements(driver_logged):
-    menu = driver_logged.find_element(By.ID, "react-burger-menu-btn")
-    filtro = driver_logged.find_element(By.CLASS_NAME, "product_sort_container")
+    assert driver_logged.menu_visible(), "El menú no está visible."
+    assert driver_logged.filtro_visible(), "El filtro no está visible."
 
-    assert menu.is_displayed(), "El icono del menu no está presente en la pagina"
-    assert filtro.is_displayed(), "El filtro del catalogo no está presente en la pagina"
