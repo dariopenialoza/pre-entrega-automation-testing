3b0e9b2f625038c07e2e60552f3a0b32aac58548
dariopenialoza <dariopenialoza@gmail.com>
2026-05-31 17:18:53 -0300

    Se agregaron dos test en teslogin

--- tests/test_login.py
+++ tests/test_login.py
@@ -14,3 +14,4 @@ def test_login_error(driver):
     login_page.login("standard_user","123456")
     error_message = login_page.get_error_password()
     assert "Epic sadface: Username and password do not match any user in this service" in error_message
+    
