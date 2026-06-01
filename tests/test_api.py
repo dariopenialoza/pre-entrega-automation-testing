7ec82b49f0f4f7360e752f9bbcbcc850d86f908d
dariopenialoza <dariopenialoza@gmail.com>
2026-06-01 20:00:48 -0300

    Se agregó test_api

---
+++ tests/test_api.py
@@ -0,0 +1,54 @@
+import requests
+
+headers = {
+    "x-api-key" : "pub_3aeea2bae00470c9e4654fe81dc7a784d411a065e0a17d8367d2e70f5003e8c6"
+}
+
+def test_login_valido():
+    body = {
+        "email": "eve.holt@reqres.in",
+        "password": "cityslicka"
+    }
+
+    response = requests.post("https://reqres.in/api/login", headers=headers,json=body)
+
+    assert response.status_code == 200
+
+def test_login_sin_password():
+    body = {
+        "email": "eve.holt@reqres.in",
+    }
+
+    response = requests.post("https://reqres.in/api/login", headers=headers,json=body)
+
+    assert response.status_code == 400
+
+def test_create_user():
+    body = {
+        "name": "Jose",
+        "email": "jose.montezuma@bue.edu.ar",
+        "password": "12345"
+    }
+
+    response = requests.post("https://reqres.in/api/users", headers=headers,json=body)
+
+    data = response.json()
+
+    assert response.status_code == 201
+
+    assert data["name"] == body["name"]
+    assert data["email"] == body["email"]
+
+    assert response.elapsed.total_seconds() < 1
+
+def test_delete_user():
+    response = requests.delete("https://reqres.in/api/users/2",headers=headers)
+
+    assert response.status_code == 204
+
+def test_get_user():
+    response = requests.get("https://reqres.in/api/users/2",headers=headers)
+
+    assert response.status_code == 200
+    print(response.elapsed.total_seconds())
+    assert response.elapsed.total_seconds() < 1, "El tiempo de ejecucion tardo mas de lo esperado"
