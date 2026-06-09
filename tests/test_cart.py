581ee60b70fa3351a0546aac8ec68ba3e594c8c3
dariopenialoza <dariopenialoza@gmail.com>
2026-06-08 21:12:12 -0300

    Actualizacion de test_cart

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
 
-def test_cart(login_in_driver):
-    driver = login_in_driver
+def test_cart(driver_logged):
+    driver = driver_logged
 
     # Agregar producto al carrito
     driver.find_elements(By.CLASS_NAME, "btn_inventory")[0].click()
